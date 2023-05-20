56547520f322d98ec42ffdc090f0e05943194db5
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-05-20 15:48:06 +0600

    find by category done

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ async function run() {
       const result = await toysInfoDb.insertOne(toysInfo);
       res.send(result);
     });
-
+    app.get("/toys-category/:category", async (req, res) => {
+      const category = req.params.category;
+      const result = await toysInfoDb.find({ category: category }).toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
