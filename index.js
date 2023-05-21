cb2e26fe1959cbfa5b78b36ac0885b10fb918a84
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-05-21 19:04:38 +0600

    deleted api created done

--- index.js
+++ index.js
@@ -73,6 +73,11 @@ async function run() {
       const result = await toysInfoDb.updateOne(filter, updatedDoc);
       res.send(result);
     });
+    app.delete("/delete-toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await toysInfoDb.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
     const createIndex = toysInfoDb.createIndex({ toyName: 1 });
     app.get("/searchByToyName/:searchText", async (req, res) => {
       const searchText = req.params.searchText;
